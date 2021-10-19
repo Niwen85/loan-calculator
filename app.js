aacfd1e630661be8f18b1d8e7c8dd6f722128312
Niklas Wendel <88709975+Niwen85@users.noreply.github.com>
2021-10-19 15:57:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function sliderChange(val) {
+    document.getElementById('sliderVal').innerHTML = val;
+}
+function sliderChange2(val2) {
+    document.getElementById('sliderVal2').innerHTML = val2;
+}
