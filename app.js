ca3ad5e3e95bde1d999e049b16077914f4b7e38d
Niklas Wendel <88709975+Niwen85@users.noreply.github.com>
2021-10-22 11:20:01 +0200

    updated loan function

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
+var yearSelect = document.getElementById('years')
+var loan = document.getElementById('sliderVal');
+var time = document.querySelector('#time');
+var slide = document.querySelector('#slider');
+var kostnad = document.querySelector('.kostnad')
 function sliderChange(val) {
-    document.getElementById('sliderVal').innerHTML = val;
+    loan.value = val;
+
+    calcYear()
 }
 function sliderChange2(val2) {
-    document.getElementById('sliderVal2').innerHTML = val2;
+    yearSelect.selectedIndex = val2
+    calcYear()
+}
+// function 1 loan /year(12)    function2 loan /100 *0.25
+
+
+loan.onchange = function () {
+    if (loan.value > 600000) {
+        loan.value = 600000
+
+    }
+    slide.value = loan.value
+
+    calcYear()
+
+}
+const calcYear = function () {
+
+    const ränta = loan.value / 100 * 0.25
+    const år = yearSelect.options[yearSelect.options.selectedIndex].value
+    const delbetalning = loan.value / (år * 12)
+    const result = Math.floor(ränta + delbetalning)
+    kostnad.innerHTML = result
+
+}
+
+function timeDropBox(val) {
+    time.value = val.index + 1
+    calcYear()
 }
